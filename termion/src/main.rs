73a09018a599908ee0b2ff721b96edfc8def1bd5
Thomas Campistron <irevoire@hotmail.fr>
2019-12-29 18:14:06 +0100

    use the other  alternative screen API

--- termion/src/main.rs
+++ termion/src/main.rs
@@ -3,13 +3,14 @@ use termion::cursor::DetectCursorPos;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
+use termion::screen::AlternateScreen;
 use termion::{color, cursor};
 
 use engine::*;
 
 macro_rules! color_print {
-    ($side:expr, $color:expr, $val:expr) => {
-        print!("{}{}{}", $side($color), $val, $side(color::Reset));
+    ($writer: expr, $side:expr, $color:expr, $val:expr) => {
+        write!($writer, "{}{}{}", $side($color), $val, $side(color::Reset)).unwrap();
     };
 }
 
@@ -17,79 +18,83 @@ fn main() {
     let mut args = args::parse();
     args.apply_fix();
     let text = args.text();
-
-    println!("{}", termion::screen::ToAlternateScreen);
-    print!("{}", termion::cursor::Save);
-    print!("{}", text);
-    print!("{}", termion::cursor::Restore);
-
     let mut result = None;
-    // the first two u16 indicates the position of the cursor, so in case of error we can go back
-    // to the end of the line. The boolean at the end indicates if there was an error during the
-    // switch of line. This way we know if we need to delete a red square.
-    let mut line_end: Vec<(u16, u16, bool)> = Vec::new();
-    let mut stdin = stdin().keys();
-    let mut stdout = stdout().into_raw_mode().unwrap();
-    stdout.flush().unwrap();
 
-    let mut engine = engine::Engine::new(&text);
-    loop {
-        stdout.flush().unwrap();
-        let c = match stdin.next() {
-            None => break,
-            Some(Ok(Key::Ctrl('c'))) => {
-                break;
-            }
-            Some(Ok(Key::Backspace)) => {
-                match engine.handle_backspace() {
-                    Running => continue,
-                    Del(n, s) => {
-                        if &s == "\n" {
-                            let return_to = line_end.pop().unwrap();
-                            print!("{}", cursor::Goto(return_to.0, return_to.1));
-                            stdout.flush().unwrap();
-                            print!(" {}", cursor::Left(1 + return_to.2 as u16));
-                        } else {
-                            print!("{}", cursor::Left(n as u16));
-                            print!("{}", s);
-                            print!("{}", cursor::Left(n as u16));
+    {
+        let mut screen = AlternateScreen::from(stdout());
+        write!(screen, "{}", termion::cursor::Save).unwrap();
+        write!(screen, "{}", text).unwrap();
+        write!(screen, "{}", termion::cursor::Restore).unwrap();
+        let mut screen = screen.into_raw_mode().unwrap();
+
+        let mut stdin = stdin().keys();
+        screen.flush().unwrap();
+
+        // the first two u16 indicates the position of the cursor, so in case of error we can go back
+        // to the end of the line. The boolean at the end indicates if there was an error during the
+        // switch of line. This way we know if we need to delete a red square.
+        let mut line_end: Vec<(u16, u16, bool)> = Vec::new();
+
+        let mut engine = engine::Engine::new(&text);
+        loop {
+            screen.flush().unwrap();
+            let c = match stdin.next() {
+                None => break,
+                Some(Ok(Key::Ctrl('c'))) => {
+                    break;
+                }
+                Some(Ok(Key::Backspace)) => {
+                    match engine.handle_backspace() {
+                        Running => continue,
+                        Del(n, s) => {
+                            if &s == "\n" {
+                                let return_to = line_end.pop().unwrap();
+                                write!(screen, "{}", cursor::Goto(return_to.0, return_to.1))
+                                    .unwrap();
+                                screen.flush().unwrap();
+                                write!(screen, " {}", cursor::Left(1 + return_to.2 as u16))
+                                    .unwrap();
+                            } else {
+                                write!(screen, "{}", cursor::Left(n as u16)).unwrap();
+                                write!(screen, "{}", s).unwrap();
+                                write!(screen, "{}", cursor::Left(n as u16)).unwrap();
+                            }
+                            continue;
                         }
-                        continue;
-                    }
-                };
+                    };
+                }
+                Some(Ok(Key::Char(c))) => c,
+                err => {
+                    println!("Unknown sequence {:?}", err);
+                    break;
+                }
+            };
+            match engine.handle_keys(c) {
+                Finished => {
+                    result = Some(engine.result());
+                    break;
+                }
+                Valid('\n') | Good('\n') => {
+                    let pos = screen.cursor_pos().unwrap();
+                    write!(screen, "{}", cursor::Goto(0, pos.1 + 1)).unwrap();
+                    line_end.push((pos.0, pos.1, false));
+                }
+                Invalid('\n') | Bad('\n') => {
+                    color_print!(screen, color::Bg, color::Red, c);
+                    let pos = screen.cursor_pos().unwrap();
+                    write!(screen, "{}", cursor::Goto(0, pos.1 + 1)).unwrap();
+                    line_end.push((pos.0, pos.1, true));
+                }
+                Invalid(' ') | Bad(' ') | Invalid(' ') | Bad(' ') => {
+                    color_print!(screen, color::Bg, color::Red, c);
+                }
+                Invalid(c) | Bad(c) => color_print!(screen, color::Fg, color::Red, c),
+                Valid(c) => color_print!(screen, color::Fg, color::Green, c),
+                Good(c) => print!("{}", c),
             }
-            Some(Ok(Key::Char(c))) => c,
-            err => {
-                println!("Unknown sequence {:?}", err);
-                break;
-            }
-        };
-        match engine.handle_keys(c) {
-            Finished => {
-                result = Some(engine.result());
-                break;
-            }
-            Valid('\n') | Good('\n') => {
-                let pos = stdout.cursor_pos().unwrap();
-                print!("{}", cursor::Goto(0, pos.1 + 1));
-                line_end.push((pos.0, pos.1, false));
-            }
-            Invalid('\n') | Bad('\n') => {
-                color_print!(color::Bg, color::Red, c);
-                let pos = stdout.cursor_pos().unwrap();
-                print!("{}", cursor::Goto(0, pos.1 + 1));
-                line_end.push((pos.0, pos.1, true));
-            }
-            Invalid(' ') | Bad(' ') | Invalid(' ') | Bad(' ') => {
-                color_print!(color::Bg, color::Red, c);
-            }
-            Invalid(c) | Bad(c) => color_print!(color::Fg, color::Red, c),
-            Valid(c) => color_print!(color::Fg, color::Green, c),
-            Good(c) => print!("{}", c),
         }
-    }
-    stdout.suspend_raw_mode().unwrap();
-    println!("{}", termion::screen::ToMainScreen);
+        screen.suspend_raw_mode().unwrap();
+    } // release the screen and come back to the main screen
 
     if result.is_some() {
         handle_result(&result.unwrap());
